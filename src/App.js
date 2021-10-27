749d12948d422d6a62bcdbab582b1af802fc1a0f
jmgcito <joshuamguillen@gmail.com>
2021-10-26 22:37:26 -0700

    wrote nextNote, noteRange, and incrementChar functions. added color to each pressed note depending on new css id. changed design of piano to make the keys float on a maroon background.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,90 @@
 import "./Piano.css";
 import React, { useState, useEffect } from "react";
 import { render } from "@testing-library/react";
-import * as Tone from 'tone';
+import * as Tone from "tone";
+
+// incrementChar("C") -> "D"
+// incrementChar("E") -> "F"
+function incrementChar(char) {
+  return String.fromCharCode(char.charCodeAt() + 1);
+}
 
 // nextNote("C3") -> "C#3"
 // nextNote("E2") -> "F2"
 function nextNote(note) {
-  if (note.length == 2) { // not sharp
-    if(note.includes(/E|G/)){ //doesn't give a sharp to E or G notes
-      return 0;
-    } else{
-      return note = note.slice(0) + '#' + note.slice(1);
+  if (note.length === 2) {
+    // not sharp
+    if (note.includes("E") || note.includes("B")) {
+      let octave = note.slice(1);
+      //doesn't add sharp to E or G
+      if (note.includes("B")) {
+        //increment octave when note returns to C
+        octave = String(parseInt(octave, 10) + 1);
+      }
+      return (note = incrementChar(note.slice(0, 1)) + octave);
+    } else {
+      //adds sharp
+      return (note = note.slice(0, 1) + "#" + note.slice(1));
     }
-  } else { //sharp
-    return 0;
+  } else {
+    //sharp
+    if (note.includes("G")) {
+      // after G#, next note is A
+      return (note = "A" + note.slice(2));
+    } else {
+      return (note = incrementChar(note.slice(0, 1)) + note.slice(2));
+    }
   }
 }
 
 // noteRange("C3", "E3") -> ["C3", "C#3", "D3", "D#3", "E3"]
 function noteRange(start, end) {
   let notes = [];
-
+  let note = start;
+  while (note !== end) {
+    notes.push(note);
+    note = nextNote(note);
+  }
   return notes;
 }
 
-
-// { ["C3", "C#3", "D3", "D#3", "E3"].map((note) => <Key note={note}, synth={synth} )}
-
-function Key(props){
-
-  return(
+function Key(props) {
+  return (
     <button
-      class={props.note.includes('#') ? 'black-key':'piano-key'} 
-      onMouseDown={()=>props.synth.triggerAttack(props.note)}
-      onMouseUp={()=> props.synth.triggerRelease('+0.2')}
+      class={props.note.includes("#") ? "black-key" : "piano-key"}
+      onMouseDown={() => props.synth.triggerAttack(props.note)}
+      onMouseUp={() => props.synth.triggerRelease("+0.2")}
+      id={
+        props.note.length === 2
+          ? props.note.charAt(0)
+          : props.note.slice(0, 1) + "s"
+      }
     />
   );
-  
 }
 
 function Piano() {
-
-  const [synth, setSynth] = useState(() => new Tone.Synth())
+  const [synth, setSynth] = useState(() => new Tone.Synth());
+  const start = "C3";
+  const end = "C6";
+  const notes = noteRange(start, end);
 
   useEffect(() => {
-    synth.toDestination()
-  }, [synth])
+    synth.toDestination();
+  }, [synth]);
 
   return (
     <div class="piano">
       <div class="piano-board">
-
-        { ["C3", "C#3", "D3", "D#3", "E3", 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3','C4', 'C#4'].map((note) => <Key note={note} synth={synth} />) }
-
-      
+        {notes.map((note) => (
+          <Key note={note} synth={synth} />
+        ))}
       </div>
     </div>
   );
 }
 
 class App extends React.Component {
-  
-
   render() {
     return <Piano />;
   }
